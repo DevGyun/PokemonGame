792c6469651e67ae7f3aa6473f6e2f76d9b43b82
DevGyun <106507093+DevGyun@users.noreply.github.com>
2023-04-10 02:51:41 +0900

    Add files via upload

--- Pokemon.java
+++ Pokemon.java
@@ -1,3 +1,36 @@
 public class Pokemon
 {
+    String name;
+    int code;
+    int level;
+    int hp;
+    int maxHp;
+    int damage;
+
+    Pokemon(int code, int level)
+    {
+        this.code = code;
+        this.level = level;
+        switch(code)
+        {
+            case 0:
+            this.name = "파이리";
+            this.maxHp = 10;
+            this.damage = 1;
+            break;
+
+            case 1:
+            this.name = "꼬북이";
+            this.maxHp = 10;
+            this.damage = 1;
+            break;
+        }
+        this.hp = this.maxHp;
+    }
+    
+    void levelUp()
+    {
+        this.level++;
+        System.out.printf("%s 의 레벨이 %d 레벨로 올랐습니다.\n", this.name, this.level);
+    }
 }
--- PokemonGame.java
+++ PokemonGame.java
@@ -5,13 +5,25 @@ public class PokemonGame
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        while(true)
+        roop:while(true)
         {
             System.out.println("--포켓몬 게임--");
             System.out.println("1 대전모드");
             System.out.println("2 수집모드");
             System.out.println("3 내정보");
             System.out.println("4 종료");
+            System.out.print(">> ");
+            switch(scanner.nextInt())
+            {
+                case 1:
+                break;
+                case 2:
+                break;
+                case 3:
+                break;
+                case 4:
+                break roop;
+            }
         }
         scanner.close();
     }
